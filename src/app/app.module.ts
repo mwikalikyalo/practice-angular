993db3ca261bde39a267e2df6f48c296633131ec
mwikalikyalo <winniemwikali07@gmail.com>
2022-10-11 12:33:59 +0300

    Add the home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [],/*add service needed in the whole app here*/
   bootstrap: [AppComponent]
 })
 export class AppModule { }
